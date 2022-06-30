1d2a720bc438839a7bf97f9882e45153147214e8
Paulo Brito <pjbrito@gmail.com>
2022-06-30 20:19:30 +0100

    init

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace MarsRoverChanllenge
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
